d84d05c5cfb5d0b2265744720f90fc1bdaa4fee5
Liam Williams <liam.a.wms@gmail.com>
2017-06-27 10:33:29 -0400

    Changed the Java portion of the coding.

--- Programming-MB-Java/src/main/CodeExecutor.java
+++ Programming-MB-Java/src/main/CodeExecutor.java
@@ -1,38 +1,28 @@
 package main;
 
-/*
- *	The purpose of this class is to run three test cases for the DateCalculator class (to be made).
- *	The only needs for the class is to have four (4) total methods for setting the date, and retrieving
- *	it in three different formats. The date needs to be stored and changeable internally, in any format.
- *
- *	@author Liam Williams
- *	@version 1
- */
+import java.util.Random;
+
 public class CodeExecutor {
 
+	char[] array = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+			'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	
 	public static void main(String[] args) {
-		DateCalculator dc = new DateCalculator(); //Creates a DateCalculator object dc
 		
-		//Test case 1.
-		dc.setDate("01/01/1998");
-		System.out.println("Date is:\n" +
-							dc.getDateExtended() + "\n" +
-							dc.getDateShortened() + "\n" +
-							dc.getDateNormal() + "\n");
-		
-		//Test case 2.
-		dc.setDate("12/30/2013");
-		System.out.println("Date is:\n" +
-							dc.getDateExtended() + "\n" +
-							dc.getDateShortened() + "\n" +
-							dc.getDateNormal() + "\n");
-		
-		//Test case 3.
-		dc.setDate("07/31/1809");
-		System.out.println("Date is:\n" +
-							dc.getDateExtended() + "\n" +
-							dc.getDateShortened() + "\n" +
-							dc.getDateNormal() + "\n");
+	}
+
+	/**
+	 * Generates a random Integer between min and max.
+	 * @param min
+	 * 			Minimum value in the range
+	 * @param max
+	 * 			Maximum value in the range
+	 * @return	Randomly generated value
+	 */
+	public static int randomInt (int min, int max) {
+		Random rand = new Random();
+		int output = min + rand.nextInt((max + 1) - min);
+		return output;
 	}
 
 }
